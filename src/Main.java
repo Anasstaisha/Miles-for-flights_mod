815ff062f9f28be5c1ff55c151351261ad8eaa31
Neto test <samarokova.a.g@bk.ru>
2022-11-15 18:06:41 +0300

    Калькулятор бонусных миль

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int price = 15_350;
+        double bonusMile = 20;
+
+        System.out.println("Стоимость Вашего билета: " + price);
+        System.out.println("Ваши бонусные мили за эту покупку: " + price / bonusMile);
+    }
+}
